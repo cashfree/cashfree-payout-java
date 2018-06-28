aa0dda61576a9f196550358cac02b45c95342d6a
preetha-in <40459012+preetha-in@users.noreply.github.com>
2018-06-28 19:08:27 +0530

    Add files via upload

---
+++ execute.java
@@ -0,0 +1,15 @@
+import cashfreeUser.*;
+
+public class execute{
+
+
+}
+
+    public static void main(String[] args) {
+        System.out.println("TELST");
+        
+        execute example = new execute();
+
+        System.out.println(example.clientAuth("!21","12","TEST"));
+
+    }
